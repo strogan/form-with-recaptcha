66076dcb0f76b54d1ec4b48c9b5e03867c1f4da1
strogan <asdof01@gmail.com>
2021-12-03 16:23:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ jQuery(document).ready(function($) {
         recaptchaResponse.value = token;
 		   
 		   
-	    
+	    /* Ajax form submit. Used ajax so page doesn't reload after submitting, has link to mail.php, so they need to in 1 folder. There is alert function,
+	    can add redirect instead of it. And after 1 sec form will clear submitted values
+	    */
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "mail.php", // Can Change to mail-smtp.php
 			data: th.serialize()
 		}).done(function() {
 			alert("Thank you!");
